229c7e7545b8551174fac06dce94ddb6ca436470
tracer-ics2017 <tracer@njuics.org>
2026-05-11 19:14:42 +0000

    >  compile 2313119 ics Linux 39d6ed9cbdfd 6.6.87.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Apr 21 17:08:54 UTC 2025 x86_64 GNU/Linux  19:14:42 up 3 days,  8:37,  0 users,  load average: 0.61, 0.45, 0.46 3b26539493ff2fe66ab8e99391f9469912b40773

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -2,10 +2,6 @@
 #include "memory/mmu.h"
 
 void raise_intr(uint8_t NO, vaddr_t ret_addr) {
-  /* TODO: Trigger an interrupt/exception with ``NO''.
-   * That is, use ``NO'' to index the IDT.
-   */
-
   vaddr_t desc_addr = cpu.idtr.base + NO * 8;
   assert(NO * 8 + 7 <= cpu.idtr.limit);
 
