68759ec5b60bb082d83863b09d0b7cee15d79fed
tracer-ics2017 <tracer@njuics.org>
2026-05-12 09:07:54 +0000

    >  run 2313119 ics Linux 39d6ed9cbdfd 6.6.87.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Apr 21 17:08:54 UTC 2025 x86_64 GNU/Linux  09:07:54 up 3 days, 12:48,  0 users,  load average: 0.30, 0.25, 0.16 5a6bea48d5262926baefcdc894c82dae6624017

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -15,6 +15,18 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit:
       _halt(a[1]);
       break;
+    case SYS_write:
+      if (a[1] == 1 || a[1] == 2) {
+        char *buf = (char *)a[2];
+        size_t len = a[3];
+        for (size_t i = 0; i < len; i ++) {
+          _putc(buf[i]);
+        }
+        r->eax = len;
+      } else {
+        r->eax = -1;
+      }
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
