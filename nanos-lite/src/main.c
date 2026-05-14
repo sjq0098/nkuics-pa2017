f349a48f17dc115eaaef725f74b04dca2c1ad834
tracer-ics2017 <tracer@njuics.org>
2026-05-14 00:50:33 +0000

    >  run 2313119 ics Linux 39d6ed9cbdfd 6.6.87.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Apr 21 17:08:54 UTC 2025 x86_64 GNU/Linux  00:50:33 up 19 min,  0 users,  load average: 0.56, 0.40, 0.26 dc8fe9b91525b19bf932e7c15ebc62bc99f852b

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,7 @@ int main() {
 
   init_fs();
 
-  uintptr_t entry = loader(NULL, "/bin/videotest");
+  uintptr_t entry = loader(NULL, "/bin/pal");
   ((void (*)(void))entry)();
 
   panic("Should not reach here");
