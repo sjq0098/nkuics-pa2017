a3b6527f9fbd530f9270c0953406c2fad6dd1260
tracer-ics2017 <tracer@njuics.org>
2026-05-20 16:49:03 +0000

    >  compile 2313119 ics Linux 39d6ed9cbdfd 6.6.87.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Apr 21 17:08:54 UTC 2025 x86_64 GNU/Linux  16:49:03 up 14:05,  0 users,  load average: 0.34, 0.25, 0.19 ab0f275d3cec955a69e1d8dfa493fab7fd3bd492

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -36,10 +36,47 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
   memcpy(guest_to_host(addr), &data, len);
 }
 
+paddr_t page_translate(vaddr_t addr, bool is_write) {
+  if (cpu.cr0.paging == 0) {
+    return addr;
+  }
+
+  uint32_t pdir_idx = addr >> 22;
+  uint32_t ptab_idx = (addr >> 12) & 0x3ff;
+  uint32_t offset = addr & PAGE_MASK;
+
+  paddr_t pdir_base = cpu.cr3.val & ~PAGE_MASK;
+  paddr_t pde_addr = pdir_base + pdir_idx * 4;
+  PDE pde;
+  pde.val = paddr_read(pde_addr, 4);
+  Assert(pde.present, "PDE not present, vaddr = 0x%08x, pdir_idx = %u", addr, pdir_idx);
+  pde.accessed = 1;
+  paddr_write(pde_addr, 4, pde.val);
+
+  paddr_t ptab_base = pde.val & ~PAGE_MASK;
+  paddr_t pte_addr = ptab_base + ptab_idx * 4;
+  PTE pte;
+  pte.val = paddr_read(pte_addr, 4);
+  Assert(pte.present, "PTE not present, vaddr = 0x%08x, ptab_idx = %u", addr, ptab_idx);
+  pte.accessed = 1;
+  if (is_write) {
+    pte.dirty = 1;
+  }
+  paddr_write(pte_addr, 4, pte.val);
+
+  return (pte.val & ~PAGE_MASK) | offset;
+}
+
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if ((addr & PAGE_MASK) + len > PAGE_SIZE) {
+    assert(0);
+  }
+  return paddr_read(page_translate(addr, false), len);
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  if ((addr & PAGE_MASK) + len > PAGE_SIZE) {
+    assert(0);
+  }
+  paddr_write(page_translate(addr, true), len, data);
 }
