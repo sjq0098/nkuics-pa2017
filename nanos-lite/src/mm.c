830a2f797b23f5b3c30c52b89e13182a013b9679
tracer-ics2017 <tracer@njuics.org>
2026-05-21 00:20:56 +0000

    >  run 2313119 ics Linux 39d6ed9cbdfd 6.6.87.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Apr 21 17:08:54 UTC 2025 x86_64 GNU/Linux  00:20:56 up 21:22,  0 users,  load average: 0.20, 0.26, 0.12 7212e97798a8e612ab8c5066f1c29ad053f6b10b

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -16,6 +16,25 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uint32_t new_brk) {
+  assert(current != NULL);
+
+  if (current->cur_brk == 0) {
+    current->cur_brk = new_brk;
+    current->max_brk = new_brk;
+    return 0;
+  }
+
+  if (new_brk > current->max_brk) {
+    uintptr_t start = PGROUNDDOWN(current->max_brk);
+    uintptr_t end = PGROUNDUP(new_brk);
+    for (uintptr_t va = start; va < end; va += PGSIZE) {
+      void *pa = new_page();
+      _map(&current->as, (void *)va, pa);
+    }
+    current->max_brk = new_brk;
+  }
+
+  current->cur_brk = new_brk;
   return 0;
 }
 
